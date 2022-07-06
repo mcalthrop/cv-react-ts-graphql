42e1e58748ed7f52a957c2d93dd96a20630d364e
Matt Calthrop <matt@calthrop.com>
2022-07-06 17:35:31 +0100

    Add Chakra

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { ApolloProvider } from '@apollo/client';
+import { ChakraProvider, Container } from '@chakra-ui/react';
 import './App.css';
 import { CvContainer } from './components/Cv';
 import { createApolloClient } from './graphql';
@@ -8,7 +9,11 @@ const apolloClient = createApolloClient();
 export function App(): JSX.Element {
   return (
     <ApolloProvider client={apolloClient}>
-      <CvContainer />
+      <ChakraProvider>
+        <Container maxWidth={'4xl'}>
+          <CvContainer />
+        </Container>
+      </ChakraProvider>
     </ApolloProvider>
   );
 }
