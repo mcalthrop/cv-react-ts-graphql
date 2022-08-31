d0e295805301f29683e746469020e6a5c8979324
Matt Calthrop <matt@calthrop.com>
2022-08-31 17:40:32 +0100

    Optimise Vite build by chunking output (#129)

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,19 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 const viteConfig = defineConfig({
+  build: {
+    // https://rollupjs.org/guide/en/#big-list-of-options
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react'],
+          reactDom: ['react-dom/client'],
+          apollo: ['@apollo/client'],
+          chakra: ['@chakra-ui/react'],
+        },
+      },
+    },
+  },
   clearScreen: false,
   envPrefix: 'REACT_APP_',
   plugins: [react()],
