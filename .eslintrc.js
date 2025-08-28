5f3a1914370ac6e6f3bd02265357ba13f9afeb21
Matt Calthrop <matt@calthrop.com>
2025-08-28 17:52:23 +0100

    Add next lint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:jest-dom/recommended',
+    'plugin:@next/next/recommended',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
