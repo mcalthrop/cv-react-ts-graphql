515a10848131b2900750bc98001a2967585f2ab2
Matt Calthrop <matt@calthrop.com>
2025-08-30 13:51:57 +0100

    Run GQL type generation before build & compile

--- scripts/fix-generated-graphql-types.sh
+++ scripts/fix-generated-graphql-types.sh
@@ -3,6 +3,6 @@
 set -xeuo pipefail
 
 GENERATED_TYPES_FILE=src/graphql-types.tsx
-GENERATED_TYPES_FILE_TMP=/tmp/$(basename ${GENERATED_TYPES_FILE}).tmp
+GENERATED_TYPES_FILE_TMP=/tmp/$(basename ${GENERATED_TYPES_FILE})
 cat ${GENERATED_TYPES_FILE} | sed "s#import \* as Apollo from '@apollo/client'#import * as Apollo from '@apollo/client/react'#" > ${GENERATED_TYPES_FILE_TMP}
 mv ${GENERATED_TYPES_FILE_TMP} ${GENERATED_TYPES_FILE}
