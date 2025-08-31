6d3567302dea16f4eaae2e37a0d4e6d670b88f22
Matt Calthrop <matt@calthrop.com>
2025-08-31 10:15:04 +0100

    Fix linting error

--- src/graphql/createApolloClient.tsx
+++ src/graphql/createApolloClient.tsx
@@ -9,15 +9,18 @@ import {
 const uri = createContentfulGraphQlUri();
 
 // Source: https://github.com/apollographql/apollo-client-integrations/tree/main/packages/nextjs#in-rsc
-export const { getClient, query, PreloadQuery } = registerApolloClient(() => new ApolloClient({
-  cache: new InMemoryCache(),
-  link: new HttpLink({
-    // this needs to be an absolute url, as relative urls cannot be used in SSR
-    uri,
-    fetchOptions: {
-      // you can pass additional options that should be passed to `fetch` here,
-      // e.g. Next.js-related `fetch` options regarding caching and revalidation
-      // see https://nextjs.org/docs/app/api-reference/functions/fetch#fetchurl-options
-    },
-  }),
-}));
+export const { getClient, query, PreloadQuery } = registerApolloClient(
+  () =>
+    new ApolloClient({
+      cache: new InMemoryCache(),
+      link: new HttpLink({
+        // this needs to be an absolute url, as relative urls cannot be used in SSR
+        uri,
+        fetchOptions: {
+          // you can pass additional options that should be passed to `fetch` here,
+          // e.g. Next.js-related `fetch` options regarding caching and revalidation
+          // see https://nextjs.org/docs/app/api-reference/functions/fetch#fetchurl-options
+        },
+      }),
+    }),
+);
