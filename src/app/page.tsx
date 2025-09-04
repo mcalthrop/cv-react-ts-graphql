8b461ed37b7063a992dc29774d95f8707300cfcd
Matt Calthrop <matt@calthrop.com>
2025-09-04 12:05:39 +0100

    Remove Suspense

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,12 @@ import { Container } from '@chakra-ui/react';
 import { CvContainer } from '@/components/CvContainer';
 import { Footer } from '@/components/Footer';
 import { Header } from '@/components/Header';
-import { Loading } from '@/components/Loading';
-import { Suspense } from 'react';
 import './page.css';
 
 const Page = (): React.JSX.Element => (
   <Container maxWidth={'4xl'}>
     <Header />
-    <Suspense fallback={<Loading />}>
-      <CvContainer />
-    </Suspense>
+    <CvContainer />
     <Footer />
   </Container>
 );
