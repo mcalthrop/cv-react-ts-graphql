37da3279718e9b66921f5bb1b84cb6d562a84a13
Matt Calthrop <matt@calthrop.com>
2025-09-04 10:48:40 +0100

    Create separate function for fetching CV data

--- src/components/CvContainer/CvContainer.tsx
+++ src/components/CvContainer/CvContainer.tsx
@@ -1,25 +1,8 @@
-import { query } from '@/graphql';
-import { GetCvDocument, type GetCvQuery } from '@/graphql/generated/graphql';
 import { CvComponent } from '@/components/CvComponent';
+import { fetchCvData } from './fetchCvData';
 
 export const CvContainer = async (): Promise<React.JSX.Element> => {
-  try {
-    const { data } = await query<GetCvQuery>({
-      query: GetCvDocument,
-    });
+  const cvFragment = await fetchCvData();
 
-    if (!data?.cvCollection || data.cvCollection.items.length < 1) {
-      return <div>No CV data found</div>;
-    }
-
-    const [cvFragment] = data.cvCollection.items;
-    if (!cvFragment) {
-      return <div>No CV data found</div>;
-    }
-
-    return <CvComponent cvFragment={cvFragment} />;
-  } catch (error) {
-    console.error('Error fetching CV data:', error);
-    return <div>Error loading CV data</div>;
-  }
+  return <CvComponent cvFragment={cvFragment} />;
 };
